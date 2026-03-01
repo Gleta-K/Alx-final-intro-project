93ea5ac122e03b5410e92c4eb27d382821ce1e06
Gleta Kijogi <gkkijogi@gmail.com>
2026-03-01 17:32:14 +0300

    modified landing screen using JSON on session_project urls

--- session_project/user_sessions/admin.py
+++ session_project/user_sessions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MonitoredApp, AppSession
 
 # Register your models here.
+admin.site.register(MonitoredApp)
+admin.site.register(AppSession)
--- session_project/user_sessions/models.py
+++ session_project/user_sessions/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
-class UserSession(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_activity = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.user.username} session"
 
 class MonitoredApp(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     app_name = models.CharField(max_length=100)
-    session_duration = models.IntegerField()  # minutes
+    session_limit_minutes = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.app_name}"
+
+
+class AppSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    monitored_app = models.ForeignKey(MonitoredApp, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.monitored_app.app_name}"
