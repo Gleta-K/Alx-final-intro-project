da52bfb9eb4ad7cccf83e4a344516c75804b3b25
Gleta Kijogi <gkkijogi@gmail.com>
2026-03-01 17:01:51 +0300

    Add API routing and SessionGuard endpoints

--- session_project/user_sessions/urls.py
+++ session_project/user_sessions/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import login_view, protected_view, logout_view
-from django.http import JsonResponse
+from .views import (
+    RegisterView,
+    LoginView,
+    AddAppView,
+    MyAppsView,
+    StartSessionView,
+    CheckSessionView
+)
 
-def api_root(request):
-    return JsonResponse({
-        "available_endpoints": [
-            "/api/login/",
-            "/api/protected/",
-            "/api/logout/"
-        ]
-    })
 urlpatterns = [
-    path('', api_root),
-    path("login/", login_view),
-    path("protected/", protected_view),
-    path("logout/", logout_view),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('add-app/', AddAppView.as_view()),
+    path('my-apps/', MyAppsView.as_view()),
+    path('start-session/', StartSessionView.as_view()),
+    path('check-session/<int:session_id>/', CheckSessionView.as_view()),
 ]
